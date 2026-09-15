698eee02cfad6100c46622e5e5fccead9dfb13ee
Ali Azhari <aliazhari@gmail.com>
2026-09-15 17:19:13 -0400

    Firstb practice

---
+++ 2026_09_15/Demo.py
@@ -0,0 +1,11 @@
+print("Hello everyone")
+print("Welcome to my Calculator")
+name = input("What's your name")
+print("Welcome " , name)
+a  = 20
+b = 10
+c = a + b
+print("a + b")
+print(c)
+
+print(c + 60)
