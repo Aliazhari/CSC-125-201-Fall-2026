a17912cd1125dbc9b0a6f5aaecb6af9f368926ae
Ali Azhari <aliazhari@gmail.com>
2026-09-17 17:21:00 -0400

    Calculator int float

---
+++ 2026_09_17/Demo.py
@@ -0,0 +1,14 @@
+# @Author: Ali Azhari
+# @Date: 09/17/2027
+# @Description: 
+# This program is about a calculator
+name = input("Enter your name: ")
+print(name)
+
+number1 = input("Enter a number 1: ")
+number2 = float(input("Enter a number 2: "))
+number1 = float(number1)
+sum = number1 + number2
+difference = number1 - number2 
+print("Sum is: ")
+print(number1 / number2)
