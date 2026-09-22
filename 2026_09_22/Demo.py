d50f6397f653b15419e3009f4daaa2124093c826
Ali Azhari <aliazhari@gmail.com>
2026-09-22 17:21:07 -0400

    int, float, variable, string

---
+++ 2026_09_22/Demo.py
@@ -0,0 +1,15 @@
+"""
+@Author: Ali Azhari
+@Filename: Demo.py
+@Date: 09/22/2026
+@Description: THis is just a tutorial
+
+"""
+# income = 1000
+# taxRate = float(input("Enter a tax rate: "))
+
+# tax = income * taxRate / 100
+
+# print("Your tax is " +  str(tax))
+
+print(0.1 + 0.2) # Output: 0.300000000000000
